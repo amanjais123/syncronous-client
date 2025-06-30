7d6094bc20a4f1e128678a516659567ebb5f3886
amanjais123 <amanjais102@gmail.com>
2025-07-01 00:46:38 +0530

    server url

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of Axios
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:"https://syncronous-server.onrender.com",
   withCredentials: true, // This ensures cookies are sent with requests
 });
 
