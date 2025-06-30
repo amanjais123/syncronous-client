74ee34154179666c29470f95613920058583c5e9
amanjais123 <amanjais102@gmail.com>
2025-07-01 01:03:25 +0530

    foxed

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of Axios
 const apiClient = axios.create({
-  baseURL:"https://syncronous-server.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // This ensures cookies are sent with requests
 });
 
